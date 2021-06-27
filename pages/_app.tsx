cb960b15bd34ea49fa8140d9fee36f47b5125300
Hekmat Hamidi <theonehamidi@gmail.com>
2021-06-27 17:38:22 -0400

    add service worker

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,25 @@ import { Header } from '../components/Header';
 import { AppProps } from 'next/app';
 import { GlobalStyle } from '../styles/global';
 import { Nav } from '../components/Nav';
+import { useEffect } from 'react';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js').then(
+          (registration) => {
+            console.log(
+              `Service worker registration successful with scope: ${registration}`
+            );
+          },
+          (error) => {
+            console.log(`Service worker registration failed: ${error}`);
+          }
+        );
+      });
+    }
+  }, []);
   return (
     <ChakraProvider>
       <GlobalStyle />
