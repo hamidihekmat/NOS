a4e012b51b87b15ac7a1a9e250a479fd9afb50b8
Hekmat Hamidi <theonehamidi@gmail.com>
2021-08-09 22:10:52 -0400

    removed service worker

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,8 @@ import { AppProps } from 'next/app';
 import { GlobalStyle } from '../styles/global';
 import { Nav } from '../components/Nav';
 import { Discord } from '../components/Discord';
-import React, { useEffect } from 'react';
-import { isChrome, isEdgeChromium } from 'react-device-detect';
+import React from 'react';
+// import { isChrome, isEdgeChromium } from 'react-device-detect';
 // Router
 import { useRouter } from 'next/router';
 import Head from 'next/head';
@@ -13,28 +13,27 @@ import Head from 'next/head';
 export default function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
   const currentPath = router.pathname.split('/');
-  useEffect(() => {
-    if (isChrome || isEdgeChromium) {
-      if ('serviceWorker' in navigator) {
-        window.addEventListener('load', () => {
-          navigator.serviceWorker.register('/sw.js').then(
-            (registration) => {
-              console.log(
-                `Service worker registration successful with scope: ${registration}`
-              );
-            },
-            (error) => {
-              console.log(`Service worker registration failed: ${error}`);
-            }
-          );
-        });
-      }
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (isChrome || isEdgeChromium) {
+  //     if ('serviceWorker' in navigator) {
+  //       window.addEventListener('load', () => {
+  //         navigator.serviceWorker.register('/sw.js').then(
+  //           (registration) => {
+  //             console.log(
+  //               `Service worker registration successful with scope: ${registration}`
+  //             );
+  //           },
+  //           (error) => {
+  //             console.log(`Service worker registration failed: ${error}`);
+  //           }
+  //         );
+  //       });
+  //     }
+  //   }
+  // }, []);
   return (
     <>
       <Head>
-        <script src="/anti-debug.js"></script>
         <title>NOS - Stream Movies & Shows</title>
       </Head>
       <ChakraProvider resetCSS>
